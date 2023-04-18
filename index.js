3278a06ee20f5effd37a05125419e53bd571f5d1
ADCarrozzo <115608798+ADCarrozzo@users.noreply.github.com>
2023-04-17 22:43:31 -0300

    agrego js

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 let slideIndex = 1;
 showSlides(slideIndex);
-
+platos = [];
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
+function popUp(){
+  alert("Pedido enviado!")
+}
+
+function validarCampos(){
+  debugger
+  if(document.getElementById("nombre").value && document.getElementById("tel").value && document.getElementById("direc").value && document.getElementById("menuEntero").value){
+    popUp();
+  }
+}
 
 function currentSlide(n) {
   showSlides(slideIndex = n);
@@ -25,4 +35,14 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+}
+
+function agregarAPedido(event,plato){
+  if(event.target.checked){
+    platos.push(plato);
+  }else{
+    platos = platos.filter(platoSeleccionado=>plato !== platoSeleccionado);
+  }
+  let elem = document.getElementById("menuEntero");
+  elem.value = platos.toString();
 }
