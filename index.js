2080f7b59b367a204a33a0e90ab108d4f263eb29
zepxtor <ezequielkpl@outlook.com>
2023-04-17 21:40:27 -0300

    inc. formulario delivery

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let slideIndex = 1;
 showSlides(slideIndex);
-
+platos = [];
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -25,4 +25,15 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+}
+
+function agregarAPedido(event,plato){
+  debugger
+  if(event.target.checked){
+    platos.push(plato);
+  }else{
+    platos = platos.filter(platoSeleccionado=>plato !== platoSeleccionado);
+  }
+  let elem = document.getElementById("menuEntero");
+  elem.value = platos.toString();
 }
